5d1a66448884f6ea20df68d9a6ab7b9cd5e579b8
mk9km <mad_matt@mail.ru>
2024-09-09 18:42:12 +0300

    Add ability for looped conversations with KeyboardInterrupt exception handling

--- 6_exception1.py
+++ 6_exception1.py
@@ -4,17 +4,33 @@
 
 Исключения: KeyboardInterrupt
 
-* Перепишите функцию hello_user() из задания while1, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
+* Перепишите функцию hello_user() из задания while1, чтобы она
+  перехватывала KeyboardInterrupt, писала пользователю "Пока!"
   и завершала работу при помощи оператора break
-    
+
 """
+
+STOP_WORD = 'хорошо'
+
 
 def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
-    
+    user_input = ''
+    print('-' * 20)
+    print(f'Hint: press CTRL + Z/X/C/F2 for interruption')
+    print('-' * 20)
+    while user_input != STOP_WORD:
+        print(' - Как дела?')
+        try:
+            user_input = input()
+        except KeyboardInterrupt:
+            break
+        user_input = user_input.lower()
+        user_input = user_input.strip()
+        print('-' * 20)
+    print(' - Пока!')
+
 if __name__ == "__main__":
     hello_user()
