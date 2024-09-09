fbe9ee64b783e16233bba95a8671f3b22a84a586
mk9km <mad_matt@mail.ru>
2024-09-09 17:52:21 +0300

    Add ability for looped conversations

--- 4_while1.py
+++ 4_while1.py
@@ -4,18 +4,29 @@
 
 Цикл while: hello_user
 
-* Напишите функцию hello_user(), которая с помощью функции input() спрашивает 
+* Напишите функцию hello_user(), которая с помощью функции input() спрашивает
   пользователя “Как дела?”, пока он не ответит “Хорошо”
-   
+
 """
+
+STOP_WORD = 'хорошо'
 
 
 def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
+    user_input = ''
+    print('-'*20)
+    print(f'Hint: enter "{STOP_WORD}" for exit')
+    print('-'*20)
+    while user_input != STOP_WORD:
+        print(' - Как дела?')
+        user_input = input()
+        user_input = user_input.lower()
+        user_input = user_input.strip()
+        print('-' * 20)
+    print(' - Bye')
 
-    
 if __name__ == "__main__":
     hello_user()
